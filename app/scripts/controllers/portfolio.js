74ce468e1b365742e2cf34703affe3902b5344d0
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2015-08-17 15:48:21 -0500

    planning comments in portfolio controller

--- app/scripts/controllers/portfolio.js
+++ app/scripts/controllers/portfolio.js
@@ -8,10 +8,23 @@
  * Controller of the gpApp
  */
 angular.module('gpApp')
-  .controller('PortfolioCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('PortfolioCtrl', ['$scope', 'budget', function ($scope, budget) {
+    
+   var b = budget.fbObj
+
+    if( b.hasOwnProperty('currency') ) { init(); } else { b.$loaded(init); } // will need to set up some double-barrelled async for this, since I'll be waiting for both budget and recipients
+
+
+    function init(){
+      
+      b.$bindTo($scope, 'b'); 
+      $scope.budget = budget;
+      // not sure I'll need to bind this to scope; I may be able to just provide it to the portfolio service or something. 
+      // In fact, maybe budget will just be a dependency for the portfolio service.
+      // Also, making budget and orgs a dependency of recipients... no, probably not.
+      // I could make a portfolio service, with budget and recipients as dependencies. yes, that feels nice.
+
+
+    }
+
   });
