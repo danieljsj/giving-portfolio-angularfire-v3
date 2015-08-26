e9601c35c773195d060bdd17210e5aa1d144dbef
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2015-08-26 15:28:14 -0500

    working with point and tooltip-point formats

--- app/scripts/services/givingchartflat.js
+++ app/scripts/services/givingchartflat.js
@@ -65,7 +65,7 @@ angular.module('gpApp')
 							distance: -30,
 							color: 'white',
 							// align: 'center', // nope; this is not text-align, it's alignment relative to the point.
-							format: '<b>{point.name}</b><br/>y: {point.y:.1f}<br/>hc percentage: {point.percentage:.1f} %<br/>portion: {point.portion:.1f}<br/>monthly: {point.monthly:.1f}<br/>yearly: {point.yearly:.1f}',
+							format: '{point.name}',
 							style: {
 								// color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
 								// textAlign: "center" // nope; looks like it only accepts a few different params, and this ain't one. i'll try actual css.
@@ -79,7 +79,31 @@ angular.module('gpApp')
 					enabled: false
 				},
 				tooltip: {
-					pointFormat: '<b>{point.name}</b><br/>y: {point.y:.1f}<br/>hc percentage: {point.percentage:.1f} %<br/>portion: {point.portion:.1f}<br/>monthly: {point.monthly:.1f}<br/>yearly: {point.yearly:.1f}'
+					// NOT WORKING IN HIGHCHARTS-NG:
+					// code exactly like this is not working: http://jsfiddle.net/gh/get/jquery/1.7.2/highslide-software/highcharts.com/tree/master/samples/highcharts/tooltip/footerformat/
+					// some related but not identical questions by other: http://stackoverflow.com/questions/22500559/is-it-possible-to-do-a-custom-rendering-with-highchart-ng
+					// 
+					// useHtml: true,
+					// headerFormat: '<small>{point.key}</small><table>',
+					// pointFormat: 
+					// 	'<tr><th colspan="2">{point.name}</th></tr>'+
+					// 	'<tr><td>% of budget:</td><td>{point.y:.1f}%</td></tr>'+
+					// 	'<tr><td>% of giving:</td><td>{point.percentage:.1f} %</td></tr>'+
+					// 	'<tr><td>monthly giving:</td><td>${point.monthly:.2f}</td></tr>'+
+					// 	'<tr><td>yearly giving:</td><td>${point.yearly:.2f}</td></tr>'
+					// ,
+					// footerFormat: '</table>'
+					// 
+					// until then, I'll stick to standard pointformat
+					headerFormat: '',
+					pointFormat: 
+						'<b>{point.name}</b>'+
+						'<br/>% of giving: <b>{point.percentage:.1f} %</td></b>'+
+						'<br/>% of budget: <b>{point.y:.1f} %</td></b>'+
+						'<br/>monthly giving: <b>$ {point.monthly:.2f}</td></b>'+
+						'<br/>yearly giving: <b>$ {point.yearly:.2f}</td</b>'
+					,
+					footerformat: ''
 				},
 				series: [
 					{
