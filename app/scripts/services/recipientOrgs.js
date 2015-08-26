06d7eb7cc0b58293d5dba2e63a0a8de0011ee77d
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2015-08-25 23:33:30 -0500

    changing org giving fields fixes their value for if budget changes.

--- app/scripts/services/recipientOrgs.js
+++ app/scripts/services/recipientOrgs.js
@@ -142,12 +142,15 @@ angular.module('gpApp')
 			this.pushOrgState(org);
 		},
 		applyChangedYearly: function(org){
+			this.selectedOrg.basis = 'amount';
 			this.applyOrgPortion( org, org.yearly / budget.yearly() );
 		},
 		applyChangedMonthly: function(org){
+			this.selectedOrg.basis = 'amount';
 			this.applyOrgPortion( org, org.monthly / budget.monthly() );
 		},
 		applyChangedPercentage: function(org){
+			this.selectedOrg.basis = 'percentage';
 			this.applyOrgPortion( org, org.percentage / 100 );
 		},
 
