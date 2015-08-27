11968120b08704e8b2b3d4308076c2c93c7b0f06
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2015-08-27 10:21:50 -0500

    flailing fails to do even some incredibly basic uses of, you know, variables and objects and binding...

--- app/scripts/controllers/sandbox.js
+++ app/scripts/controllers/sandbox.js
@@ -8,8 +8,39 @@
  * Controller of the gpApp
  */
 angular.module('gpApp')
-  .controller('SandboxCtrl', function ($scope, taxonomization) {
+  .controller('SandboxCtrl', function ($scope, Ref, /*taxonomization*/ $firebaseObject, Auth) {
 
-  	console.log(taxonomization);
+	// taxonomization.taxTree.$bindTo($scope, 'taxTree', true);
+	// $scope.taxn = taxonomization;
+
+    var uid = Auth.$getAuth().uid;
+
+    var query = Ref.child('userTaxonomizationTrees/'+uid);
+
+    var taxTreeVar = $firebaseObject(query);
+
+    // $scope.taxTreeVar = taxTreeVar;
+
+    taxTreeVar.$bindTo($scope, 'taxTree', true);
+
+    window.taxTreeVar = taxTreeVar;
+
+	taxTreeVar.taxonomies = [{name: "tax1"}];
+	console.log('taxTreeVar immediately after `taxTreeVar.taxonomies = [{name: "tax1"}];`',taxTreeVar);
+	// wow. it just doesn't even let that new array in there...
+	// AHA! it IS letting it in! Just for a teeny weeny second; it's showing up in the instantaneous text-summary of the object, but by the time Chrome can make the dropdown, it's GONE.
+
+	taxTreeVar.taxonomies = "foo";
+	console.log('taxTreeVar immediately after `taxTreeVar.taxonomies = "foo";`',taxTreeVar);
+	// wait a blooming second here... it's not even accepting "foo"?
+
+
+    $scope.addTax = function(){
+		taxTreeVar.taxonomies.push({name:"New Taxonomy"});
+	};
+
+    $scope.addTermTo = function(tax){
+		tax.terms.push({name:"New Term"});
+	};
 
   });
