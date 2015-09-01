45830a9622732cae4bb177895410ba501e505f5a
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2015-09-01 11:58:56 -0500

    moving stuff from sandbox out into service. haven't shredded the code just yet though

--- app/scripts/controllers/categories.js
+++ app/scripts/controllers/categories.js
@@ -8,77 +8,24 @@
  * Controller of the gpApp
  */
 angular.module('gpApp')
-  .controller('CategoriesCtrl', function ($scope, Ref, /*taxonomization*/ $firebaseObject, Auth) {
-
-  	function randId(){return Math.round((Math.random()*.9+0.1) * 10000000000000000)}
+  .controller('CategoriesCtrl', function ($scope, Ref, taxonomization, $firebaseObject, Auth) {
 
 	// taxonomization.taxTree.$bindTo($scope, 'taxTree', true);
 	// $scope.taxn = taxonomization;
 
-    var uid = Auth.$getAuth().uid;
-
-    var query = Ref.child('userTaxonomizationTrees/'+uid);
-
-    var taxTreeInitial = $firebaseObject(query); // has browser-known things like User, but doesn't know any fb data yet.
-    var taxTree = {};
-
-    taxTreeInitial.$loaded(function(loadedTaxTree){
-    	taxTree = loadedTaxTree;
-    	console.log("TAX TREE LOADED");
-    	init();
-    });
-
-    function init(){
-
-	    console.log('taxTree at init: ',taxTree.taxonomies);
-	    console.log('taxTree.taxonomies at init: ',taxTree.taxonomies);
-
-	    if (! taxTree.taxonomies){
-
-		    taxTree.taxonomies = {};
-		    var newTax;
-
-		    // TODO: MAYBE: replace this basic method with some 'readonly' / 'fixed' terms, like mint has.
-		    
-		    newTax = {name: "Locality", terms: {} };
-			newTax.terms[randId()] = {name: "Local"},
-			newTax.terms[randId()] = {name: "State"},
-			newTax.terms[randId()] = {name: "National"},
-			newTax.terms[randId()] = {name: "International"}
-			taxTree.taxonomies[randId()] = newTax;
-
-			newTax = {name: "Modality", terms: {} };
-			newTax.terms[randId()] = {name: "Relief"},
-			newTax.terms[randId()] = {name: "Advocacy"},
-			newTax.terms[randId()] = {name: "Development"},
-			newTax.terms[randId()] = {name: "Education"},
-			newTax.terms[randId()] = {name: "Support"},
-			taxTree.taxonomies[randId()] = newTax;
-
-	    }
-
-	    taxTree.$save(); // WHOAH! this was being necessary to load stuff into page... and $scope.$apply() wasn't cutting it!
 
 
-	    $scope.taxTree = taxTree;
+		bindTaxTree = function(taxTree){
 
-	    $scope.taxn = { // eventually this will go live in its own service
-	    	addNewTax: function(){
-	    		var newTerms = {};
-	    		newTerms[randId()] = {name:""}; 
-				$scope.taxTree.taxonomies[randId()] = {name:"", terms:newTerms }; // WHOAH! I had to do this to the $scope.thing version, or it wouldn't see the change! But.. but... 
-				// $scope.$digest(); // $apply already in progress, because ng-click triggers it.
-				console.log(taxTree);
-			},
-			addNewTermTo: function(tax){
-				tax.terms[randId()] = {name:""};
-				// $scope.$digest(); // $apply already in progress, because ng-click triggers it.
-			}
+		    $scope.taxTree = taxTree;
+
+		    taxTree.$bindTo($scope, 'taxTree', true );
+
+		    window.taxTree = taxTree;
+
 		};
 
-	    taxTree.$bindTo($scope, 'taxTree', true );
-
-	    window.taxTree = taxTree;
+		$scope.taxn = new Taxonomization(bindTaxTree);
 
 		// taxTree.taxonomies = [{name: "tax1"}];
 		// console.log('taxTree immediately after `taxTree.taxonomies = [{name: "tax1"}];`',taxTree);
@@ -88,6 +35,5 @@ angular.module('gpApp')
 		// taxTree.taxonomies = "foo";
 		// console.log('taxTree immediately after `taxTree.taxonomies = "foo";`',taxTree);
 		// // wait a blooming second here... it's not even accepting "foo"?
-    }
 
   });
--- app/scripts/services/taxonomization.js
+++ app/scripts/services/taxonomization.js
@@ -10,22 +10,67 @@
 angular.module('gpApp')
   .service('taxonomization', function (Auth, Ref, $firebaseObject) {
     // AngularJS will instantiate a singleton by calling "new" on this function
-    
-    // "taxs" is short of "taxonomies"
+
 
     var uid = Auth.$getAuth().uid;
 
     var query = Ref.child('userTaxonomizationTrees/'+uid);
 
-    return { 
-    	taxTree: new $firebaseObject(query),
-    	addTax: function(){
-    		if ( ! this.taxTree ) { this.taxTree = []; }
-    		if ( ! this.taxTree.taxonomies ) { this.taxTree.taxonomies = []; }
-    		this.taxTree.taxonomies.push({name:"New Taxonomy"});
-    	},
-       	addTermTo: function(tax){
-    		tax.terms.push({name:"New Term"});
-    	}
-    };
+    var taxTreeInitial = $firebaseObject(query); // has browser-known things like User, but doesn't know any fb data yet.
+    var taxTree = {};
+
+    taxTreeInitial.$loaded(function(loadedTaxTree){
+        taxTree = loadedTaxTree;
+        console.log("TAX TREE LOADED");
+        init();
+    });
+
+    function init(){
+
+        console.log('taxTree at init: ',taxTree.taxonomies);
+        console.log('taxTree.taxonomies at init: ',taxTree.taxonomies);
+
+        if (! taxTree.taxonomies){
+
+            taxTree.taxonomies = {};
+            var newTax;
+
+            newTax = {name: "Locality", terms: {} };
+            newTax.terms[randId()] = {name: "Local"},
+            newTax.terms[randId()] = {name: "State"},
+            newTax.terms[randId()] = {name: "National"},
+            newTax.terms[randId()] = {name: "International"}
+            taxTree.taxonomies[randId()] = newTax;
+
+            newTax = {name: "Modality", terms: {} };
+            newTax.terms[randId()] = {name: "Relief"},
+            newTax.terms[randId()] = {name: "Advocacy"},
+            newTax.terms[randId()] = {name: "Development"},
+            newTax.terms[randId()] = {name: "Education"},
+            newTax.terms[randId()] = {name: "Support"},
+            taxTree.taxonomies[randId()] = newTax;
+
+        }
+
+        taxTree.$save(); // WHOAH! this was being necessary to load stuff into page... and $scope.$apply() wasn't cutting it!
+
+
+
+        var addNewTax = function(){
+            var newTerms = {};
+            newTerms[randId()] = {name:""}; 
+            $scope.taxTree.taxonomies[randId()] = {name:"", terms:newTerms };       // WHOAH! I had to do this to the $scope.thing version, or it wouldn't see the change! But.. but... 
+                                                                                    // nope: $scope.$digest(); // $apply already in progress, because ng-click triggers it.
+            console.log(taxTree);
+        };
+        var addNewTermTo = function(tax){
+            tax.terms[randId()] = {name:""};
+                                                                                        // nope: $scope.$digest(); // $apply already in progress, because ng-click triggers it.
+        };
+    }
+
+    return function(taxTreeReadyCallback){
+
+    }
+
   });
