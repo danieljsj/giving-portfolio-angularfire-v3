9e690725bd0230376798e536bf45270909bffc14
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2015-09-01 14:35:53 -0500

    yo giving-charts-common service

---
+++ app/scripts/services/givingchartscommon.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name gpApp.givingchartscommon
+ * @description
+ * # givingchartscommon
+ * Service in the gpApp.
+ */
+angular.module('gpApp')
+  .service('givingchartscommon', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
