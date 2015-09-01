0e56dee4e68b6c370d99e765c7fbab9cb07de235
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2015-09-01 10:13:56 -0500

    taxonomy uses randomly generated ids, binds upon load

--- app/scripts/controllers/sandbox.js
+++ app/scripts/controllers/sandbox.js
@@ -7,8 +7,11 @@
  * # SandboxCtrl
  * Controller of the gpApp
  */
+
 angular.module('gpApp')
   .controller('SandboxCtrl', function ($scope, Ref, /*taxonomization*/ $firebaseObject, Auth) {
+
+  	function randId(){return Math.round((Math.random()*.9+0.1) * 10000000000000000)}
 
 	// taxonomization.taxTree.$bindTo($scope, 'taxTree', true);
 	// $scope.taxn = taxonomization;
@@ -17,61 +20,70 @@ angular.module('gpApp')
 
     var query = Ref.child('userTaxonomizationTrees/'+uid);
 
-    var taxTreeVar = $firebaseObject(query);
+    var taxTreeInitial = $firebaseObject(query); // has browser-known things like User, but doesn't know any fb data yet.
+    var taxTree = {};
 
-    console.log('taxTreeVar.taxonomies: ',taxTreeVar.taxonomies);
+    taxTreeInitial.$loaded(function(loadedTaxTree){
+    	taxTree = loadedTaxTree;
+    	console.log("TAX TREE LOADED");
+    	init();
+    });
 
-    // if (! taxTreeVar.taxonomies){
+    function init(){
 
-	    taxTreeVar.taxonomies = {
-	    	tax1: {
-	    		name: "Locality",
-	    		terms: [
-	    			{name: "Local"},
-	    			{name: "State"},
-	    			{name: "National"},
-	    			{name: "International"}
-	    		]
-	    	},
-	    	tax2: {
-	    		name: "Modality",
-	    		terms: [
-	    			{name: "Relief"},
-	    			{name: "Advocacy"},
-	    			{name: "Development"}
-	    		]
-	    	}
-	    };
+	    console.log('taxTree at init: ',taxTree.taxonomies);
+	    console.log('taxTree.taxonomies at init: ',taxTree.taxonomies);
 
-    // }
+	    if (! taxTree.taxonomies){
 
-    taxTreeVar.$save(); // DEV... wait... whoah... this was being necessary! probably just for its $apply();
+		    taxTree.taxonomies = {};
+		    var newTax;
+		    
+		    newTax = {name: "Locality", terms: {} };
+			newTax.terms[randId()] = {name: "Local"},
+			newTax.terms[randId()] = {name: "State"},
+			newTax.terms[randId()] = {name: "National"},
+			newTax.terms[randId()] = {name: "International"}
+			taxTree.taxonomies[randId()] = newTax;
+
+			newTax = {name: "Modality", terms: {} };
+			newTax.terms[randId()] = {name: "Relief"},
+			newTax.terms[randId()] = {name: "Advocacy"},
+			newTax.terms[randId()] = {name: "Development"},
+			newTax.terms[randId()] = {name: "Education"},
+			newTax.terms[randId()] = {name: "Support"},
+			taxTree.taxonomies[randId()] = newTax;
+
+	    }
+
+	    taxTree.$save(); // WHOAH! this was being necessary to load stuff into page... and $scope.$apply() wasn't cutting it!
 
 
+	    // $scope.taxTree = taxTree;
 
+	    taxTree.$bindTo($scope, 'taxTree', true );
 
-    // $scope.taxTreeVar = taxTreeVar;
+	    window.taxTree = taxTree;
 
-    taxTreeVar.$bindTo($scope, 'taxTree' );
+		// taxTree.taxonomies = [{name: "tax1"}];
+		// console.log('taxTree immediately after `taxTree.taxonomies = [{name: "tax1"}];`',taxTree);
+		// // wow. it just doesn't even let that new array in there...
+		// // AHA! it IS letting it in! Just for a teeny weeny second; it's showing up in the instantaneous text-summary of the object, but by the time Chrome can make the dropdown, it's GONE.
 
-    window.taxTreeVar = taxTreeVar;
-
-	taxTreeVar.taxonomies = [{name: "tax1"}];
-	console.log('taxTreeVar immediately after `taxTreeVar.taxonomies = [{name: "tax1"}];`',taxTreeVar);
-	// wow. it just doesn't even let that new array in there...
-	// AHA! it IS letting it in! Just for a teeny weeny second; it's showing up in the instantaneous text-summary of the object, but by the time Chrome can make the dropdown, it's GONE.
-
-	taxTreeVar.taxonomies = "foo";
-	console.log('taxTreeVar immediately after `taxTreeVar.taxonomies = "foo";`',taxTreeVar);
-	// wait a blooming second here... it's not even accepting "foo"?
+		// taxTree.taxonomies = "foo";
+		// console.log('taxTree immediately after `taxTree.taxonomies = "foo";`',taxTree);
+		// // wait a blooming second here... it's not even accepting "foo"?
+    }
 
 
     $scope.addTax = function(){
-		taxTreeVar.taxonomies.push({name:"New Taxonomy"});
+		taxTree.taxonomies[randId()] = {name:"New Taxonomy"};
+		$scope.$digest();
 	};
 
     $scope.addTermTo = function(tax){
-		tax.terms.push({name:"New Term"});
+		tax.terms[randId()] = {name:"New Term"};
+		$scope.$digest();
 	};
 
   });
