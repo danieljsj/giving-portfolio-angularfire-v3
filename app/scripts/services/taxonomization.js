7d64c1f853391f8df9fce522cd7f2786e34ab624
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2015-09-06 09:28:57 -0500

    getting to where taxs and terms have ids matching their index in containing dictionaries

--- app/scripts/services/taxonomization.js
+++ app/scripts/services/taxonomization.js
@@ -28,26 +28,39 @@ angular.module('gpApp')
         console.log('loadedTaxTree at init: ',loadedTaxTree.taxonomies);
         console.log('loadedTaxTree.taxonomies at init: ',loadedTaxTree.taxonomies);
 
-        if (! loadedTaxTree.taxonomies){
+        var taxs = loadedTaxTree.taxonomies;
 
-            loadedTaxTree.taxonomies = {};
+        if (! taxs){
+
+            taxs = {};
             var newTax;
 
             newTax = {name: "Locality", terms: {} };
-            newTax.terms[randId()] = {name: "Local"},
-            newTax.terms[randId()] = {name: "State"},
-            newTax.terms[randId()] = {name: "National"},
-            newTax.terms[randId()] = {name: "International"}
-            loadedTaxTree.taxonomies[randId()] = newTax;
+            newTax.terms[randId()] = {id:lastId(), name: "Local"},
+            newTax.terms[randId()] = {id:lastId(), name: "State"},
+            newTax.terms[randId()] = {id:lastId(), name: "National"},
+            newTax.terms[randId()] = {id:lastId(), name: "International"}
+            taxs[randId()] = newTax;
+            taxs[lastId()].id = lastId();
 
             newTax = {name: "Modality", terms: {} };
-            newTax.terms[randId()] = {name: "Relief"},
-            newTax.terms[randId()] = {name: "Advocacy"},
-            newTax.terms[randId()] = {name: "Development"},
-            newTax.terms[randId()] = {name: "Education"},
-            newTax.terms[randId()] = {name: "Support"},
-            loadedTaxTree.taxonomies[randId()] = newTax;
+            newTax.terms[randId()] = {id:lastId(), name: "Relief"},
+            newTax.terms[randId()] = {id:lastId(), name: "Advocacy"},
+            newTax.terms[randId()] = {id:lastId(), name: "Development"},
+            newTax.terms[randId()] = {id:lastId(), name: "Education"},
+            newTax.terms[randId()] = {id:lastId(), name: "Support"},
+            taxs[randId()] = newTax;
+            taxs[lastId()].id = lastId();
 
+            loadedTaxTree.taxonomies = taxs;
+        }
+
+        // assuring that all taxs and terms in dictionaries have id properties that match their dictionary index.
+        for (var taxId in taxs){
+            taxs[taxId].id = taxId;
+            for (var termId in taxs.terms){
+                taxs.terms[termId].id = termId;
+            }
         }
 
         // lets see if it works from in here...
@@ -57,8 +70,8 @@ angular.module('gpApp')
 
     this.addNewTax = function(){
         var newTerms = {};
-        newTerms[randId()] = {name:""}; 
-        this.taxTree.taxonomies[randId()] = {name:"", terms:newTerms };       // WHOAH! you have to use $scope.thing[key] (vs this.thing[key]), or the scope wouldn't see the change! Weird because down below, the addTerm changes are seen by $scope/$digest... heh... I bet i could do a $save on it :)
+        newTerms[randId()] = {id:lastId(), name:""}; 
+        this.taxTree.taxonomies[randId()] = {id:lastId(), name:"", terms:newTerms };       // WHOAH! you have to use $scope.thing[key] (vs this.thing[key]), or the scope wouldn't see the change! Weird because down below, the addTerm changes are seen by $scope/$digest... heh... I bet i could do a $save on it :)
         // nope: $scope.$digest(); // $apply already in progress, because ng-click triggers it.
         this.taxTree.$save(); // lol. not sure why, as noted above, this is needed for adding the prop to the top-level $scope item (tax), but not needed for adding deeper items (terms)....
         console.log(this.taxTree);
