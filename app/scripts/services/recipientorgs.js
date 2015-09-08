eeaa638e9fa021dd9310caf8cbe03a994677dd5f
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2015-09-08 16:47:13 -0500

    BROKEN: separating layers; data, methods. for Orgs.

--- app/scripts/services/recipientorgs.js
+++ app/scripts/services/recipientorgs.js
@@ -8,8 +8,8 @@
  * Service in the gpApp.
  */
 angular.module('gpApp')
-  .service('recipientOrgs', ['Auth', 'Ref', '$firebaseArray',    // when using 'user', Unknown provider: userProvider
-                   function ( Auth,   Ref,   $firebaseArray ) {
+  .service('recipientOrgs', ['Auth', 'Ref', '$firebaseArray', 'recipientOrgsMethods',    // when using 'user', Unknown provider: userProvider
+                   function ( Auth,   Ref,   $firebaseArray,   orgsMethods ) {
     // AngularJS will instantiate a singleton by calling "new" on this function
     
     var uid = Auth.$getAuth().uid;
@@ -18,6 +18,35 @@ angular.module('gpApp')
 	
 	var orgs = $firebaseArray(query);
 
-	return 
+	orgs.$loaded() 	//https://www.firebase.com/docs/web/libraries/angular/api.html#angularfire-firebasearray-loaded
+	  .then(function(loadedOrgs) {
+		if (loadedOrgs === orgs ){
+			for (var methodName in orgsMethods){
+				orgs[methodName] = orgsMethods[methodName].bind(orgs);
+			}				
+			// angular.extend(orgs,OrgsCollectionFuncs);   // decided not to go with angular.extend, I guess... // under angular.extend, the resulting `this` for the funcs, I believe, was pointing to the "var OrgsCollectionFuncs = {}" object; so not going to do the extending here.
 
+			// ctrlSaveToScopeOrgsThenInit(orgs); // this was for when I was pulling them in weird-wise into the controller"
+		}
+	  })
+	  .catch(function(error) {
+		console.log("Error:", error);
+	  });
+
+	return orgs;
+
+
+
+
+		// orgsFbArray.$loaded().catch(function(err) { console.error(err); });
+		
+		// not even sure I need that ..
+		// 
+		// 
+  //   	orgsFbArray.$loaded( // $   // $loaded() 	Returns a promise which resolves after the initial records have been downloaded from our database. This is only called once and should be used with care. See Extending the Services for more ways to hook into server events. 
+  //   		asOrgsCollection.bind(orgsFbArray) // if I don't mind letting my controller know about the service, I can just have the controller do the $loaded(asOrgsCollection) thing
+  //   	).catch(function(err) {
+		//	console.error(err);
+		// });
+		// 
   }]);
--- app/scripts/services/recipientorgsmethods.js
+++ app/scripts/services/recipientorgsmethods.js
@@ -19,9 +19,7 @@ angular.module('gpApp')
 
 
 
-	var orgsFuncs = {
-
-
+	return {
 
 		// CREATION, SAVING, DELETING:
 		
@@ -201,40 +199,6 @@ angular.module('gpApp')
 			return totalMonthlyGiving / budget.monthly() * 100;
 		}
 
-	}
+	};
 
-
-
-
-	// I really want to do this async-initialization stuff differently, i.e. have the thing actually return the promise... but I'm not sure if I'll be able to, and I don't want to pick extra fights just yet...
-
-	this.getOrgs = function(ctrlSaveToScopeOrgsThenInit){
-    	
-		
-		orgs.$loaded() 	//https://www.firebase.com/docs/web/libraries/angular/api.html#angularfire-firebasearray-loaded
-		  .then(function(loadedOrgs) {
-			if (loadedOrgs === orgs ){
-				for (var funcName in orgsFuncs){
-					orgs[funcName] = orgsFuncs[funcName].bind(orgs);
-				}				
-				// angular.extend(orgs,OrgsCollectionFuncs);   // decided not to go with angular.extend, I guess... // under angular.extend, the resulting `this` for the funcs, I believe, was pointing to the "var OrgsCollectionFuncs = {}" object; so not going to do the extending here.
-
-				ctrlSaveToScopeOrgsThenInit(orgs);
-			}
-		  })
-		  .catch(function(error) {
-			console.log("Error:", error);
-		  });
-	}
-
-		// orgsFbArray.$loaded().catch(function(err) { console.error(err); });
-		
-		// not even sure I need that ..
-		// 
-		// 
-  //   	orgsFbArray.$loaded( // $   // $loaded() 	Returns a promise which resolves after the initial records have been downloaded from our database. This is only called once and should be used with care. See Extending the Services for more ways to hook into server events. 
-  //   		asOrgsCollection.bind(orgsFbArray) // if I don't mind letting my controller know about the service, I can just have the controller do the $loaded(asOrgsCollection) thing
-  //   	).catch(function(err) {
-		//	console.error(err);
-		// });
   }]);
